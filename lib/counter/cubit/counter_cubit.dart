f000cf42a9b2a8c7075a110a1acc34b2f9dad907
Timothy Ademola <timife007@gmail.com>
2023-08-22 11:02:22 +0100

    counter implemented

---
+++ lib/counter/cubit/counter_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+
+/// A [Cubit] which manages an [int] as its state.
+/// {@endtemplate}
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+
+  void decrement() => emit(state - 1);
+}
