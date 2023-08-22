f931435f36c4986d41e673e5efb92b0c644b67ca
Timothy Ademola <timife007@gmail.com>
2023-08-22 10:39:36 +0100

    counter observer impl

---
+++ lib/counter_observer.dart
@@ -0,0 +1,14 @@
+
+import 'package:bloc/bloc.dart';
+
+// To observe all state changes in the app in one place.
+class CounterObserver extends BlocObserver {
+  const CounterObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    // ignore: avoid_print
+    print('${bloc.runtimeType} $change');
+  }
+}
